52a6b5f69fa0efc3c8fe46c481dd1a064aeaa392
donn0n <don_n0n@mail.ru>
2025-05-24 21:31:22 +0600

    Update content for DotA ICCUP maphack

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "126687b34c75a921"; }
+/******/ 		__webpack_require__.h = function() { return "60ccc3ec6b2b30e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/global/hero.js
+++ src/components/global/hero.js
@@ -18,53 +18,42 @@ import { useTranslation } from 'react-i18next';
 
 export function SiteHero() {
     const { t } = useTranslation();
-    //sürekli değişen yazı
     const textarray = [
-       {text: "Counter-Strike 1.2", color: "text-red-500", src: "https://cdn.discordapp.com/attachments/1165441669831856209/1166900913894608957/NicePng_csgo-ct-png_2279334.png?ex=654c2c29&is=6539b729&hm=4c158e64337aaa558bad07ed352dce6fe3e666100c9ae31abc7ef13f5343221c&", url: "https://www.youtube.com/watch?v=FFaQWiLZx70"},
-       {text: "Counter-Strike 1.3", color: "text-red-500", src: "https://cdn.discordapp.com/attachments/1165441669831856209/1166900913894608957/NicePng_csgo-ct-png_2279334.png?ex=654c2c29&is=6539b729&hm=4c158e64337aaa558bad07ed352dce6fe3e666100c9ae31abc7ef13f5343221c&" , url: "https://www.youtube.com/watch?v=FFaQWiLZx70"},
-       {text: "Counter-Strike 1.4", color: "text-red-500", src: "https://cdn.discordapp.com/attachments/1165441669831856209/1166900913894608957/NicePng_csgo-ct-png_2279334.png?ex=654c2c29&is=6539b729&hm=4c158e64337aaa558bad07ed352dce6fe3e666100c9ae31abc7ef13f5343221c&" , url: "https://www.youtube.com/watch?v=FFaQWiLZx70"},
+       {text: "DotA ICCUP", color: "text-red-500", src: "https://cdn.discordapp.com/attachments/1165441669831856209/1166900913894608957/NicePng_csgo-ct-png_2279334.png?ex=654c2c29&is=6539b729&hm=4c158e64337aaa558bad07ed352dce6fe3e666100c9ae31abc7ef13f5343221c&", url: "https://www.youtube.com/watch?v=FFaQWiLZx70"},
     ]
     const cheattags = [
-        {text: "Aimbot"},
-        {text: "Wallhack"},
-        {text: "No Recoil"},
-        {text: "No Spread"},
-        {text: "No Flash"},
-        {text: "No Smoke"},
-        {text: "Bunnyhop"},
-        {text: "ESP"},
-        {text: "Radar"},
-        {text: "Triggerbot"},
+        {text: "Fog Reveal"},
+        {text: "Ward Spots"},
+        {text: "Rune Control"},
+        {text: "Creep Timer"},
+        {text: "Tower Range"},
+        {text: "Enemy Items"},
+        {text: "Illusion Detector"},
+        {text: "Skill Cooldowns"},
+        {text: "Minimap Icons"},
+        {text: "Roshan Timer"},
     ]
 
-    const gamecategory = [
-        "Counter-Strike 1.6",
-        "Counter-Strike: Condition Zero",
-        "Counter-Strike: Source",
-        "Counter-Strike: Global Offensive",
-        "Counter-Strike: Online",
-        "Counter-Strike: Online 2",
-        "Counter-Strike Nexon: Zombies",
-        "Counter-Strike Neo",
-    ]
-    
     const [text, setText] = useState(textarray[0].text);
     const [color, setColor] = useState(textarray[0].color);
     const [src, setSrc] = useState(textarray[0].src);
     const [index, setIndex] = useState(0);
      
-  
-
     return <>
-       
-       <div className=" dark:bg-accent/25 relative overflow-hidden rounded-lg mt-5 bg-black/25 p-3 sm:p-10 mx-4 sm:mx-0">
+       <div className="dark:bg-accent/25 relative overflow-hidden rounded-lg mt-5 bg-black/25 p-3 sm:p-10 mx-4 sm:mx-0">
       <div className="container mx-auto flex items-center justify-center">
         <div className="w-full text-center lg:text-left">
-            <h1 className="text-3xl font-bold   text-gray-900 truncate dark:text-white !z-10">
+            <h1 className="text-3xl font-bold text-gray-900 truncate dark:text-white !z-10">
                 {text}
             </h1>
-            <p className="text-gray-600 dark:text-gray-500 my-6 text-sm">
-                {t('cheats.description')}
+            <p className="text-gray-600 dark:text-gray-500 my-6">
+                Приобретая подписку на месяц и выше, вы получаете эксклюзивный доступ к "MapHack VIP Club", а также:
+                <br/><br/>
+                ✅ Установку мапхака: Наша команда профессионалов установит мапхак на вашем компьютере через AnyDesk, гарантируя правильную настройку для ICCUP.
+                <br/><br/>
+                ✅ Оптимизацию Warcraft 3: Мы проведем оптимизацию игры для DotA, чтобы ваш игровой процесс стал ещё более плавным и приятным.
+                <br/><br/>
+                ⚙️ Компенсацию за проблемы: Мы ценим ваше доверие и гарантируем, что в случае возникновения проблем с работой нашей программы, вы получите компенсацию в виде дополнительного времени использования или расширения подписки.
             </p>
            
             <div className="flex flex-wrap gap-1">
@@ -74,8 +63,6 @@ export function SiteHero() {
                     </div>
                 ))}
             </div>
-            
-
 
             <Link href={textarray[index].url}> 
             <Button
@@ -93,7 +80,6 @@ export function SiteHero() {
         </div>
         <div className="absolute top-0 left-0 h-64 -z-10 w-64 bg-accent/50 rounded-full -m-40 -mb-56 dark:bg-accent-dark"></div>
         <div className="absolute bottom-0 right-0 -z-10 h-64 w-64 bg-accent/50 rounded-full -m-32 -mb-48 dark:bg-accent-dark"></div>
-  
     </div>
     </>
 }
